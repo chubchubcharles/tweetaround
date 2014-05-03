09af1dee3519b4d340c7dcf94f0a44cb59c20ea5
Charles <charles@charles-UX32VD.(none)>
2014-05-03 11:23:22 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweetaround::Application.config.secret_key_base = 'ef74c2e5f799b18a321497d072ce958d9502a9e10305be7c51d1c2e5bec1768d7f81937a252114856a3cdf4d72008085c47cc1894a43fec912a3039b14706a1a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tweetaround::Application.config.session_store :cookie_store, key: '_tweetaround_session'
