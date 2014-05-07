882fce746320fc83a77fb42abcd63b9db27ce6c1
Charles Liu <che-yuan.liu@mail.mcgill.ca>
2014-05-07 15:04:44 -0400

    added final_query for testing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,20 @@
 class SearchController < ApplicationController
 	def index
-		@hashtag = params[:q]
+		@query = params[:q]
+
+		#test for montreal,canada
+		@geocode = "45.505730%2C-73.579928%"
+
+		#structuring query
+		@base_query = "https://api.twitter.com/1.1/search/tweets.json?"
+		@query_query = "q=" + @query
+		@geocode_query = "&geocode=" + @geocode 
+		@tail_query = "2C1mi&result_type=recent"
+
+		@final_query = @base_query + @query_query + @geocode_query + @tail_query
+
+		#send query to twitter api
+
 		
 		#for each major city
 
