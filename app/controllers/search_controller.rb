aad7d1e54cb455c3925fe0128fb6b54660c31897
Charles Liu <che-yuan.liu@mail.mcgill.ca>
2014-05-12 17:16:10 -0400

    twitter request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -73,6 +73,8 @@ class SearchController < ApplicationController
       client = Twitter::REST::Client.new do |config|
         config.consumer_key     = ENV["TWITTER_CONSUMER_KEY"]
         config.consumer_secret    = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token     = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
         config.bearer_token     = ENV["TWTTER_BEARER_TOKEN"]
       end 
       # @client_user = client.user("charlesliu2012")
