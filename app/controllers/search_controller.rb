c4b25b7fcb1c5316819553d3a6f34888778bbcea
Charles Liu <che-yuan.liu@mail.mcgill.ca>
2014-05-13 16:56:07 -0400

    added toronto and montreal as sample cities on gmap testing page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
   def index
     @address = Array.new
     @geocode_location = Array.new
+    @name_to_lat = Hash.new
+    @name_to_lng = Hash.new
     @name_to_urls = Hash.new
 
     require 'rest-client'
@@ -12,8 +14,8 @@ class SearchController < ApplicationController
       res = RestClient.get(URI.encode("#{@base_google_url}#{@address}"))
       parsed_res = Crack::XML.parse(res) #parsing XML 
       begin
-        lat = parsed_res["GeocodeResponse"]["result"]["geometry"]["location"]["lat"]
-        lng = parsed_res["GeocodeResponse"]["result"]["geometry"]["location"]["lng"]
+        @name_to_lat[address] = parsed_res["GeocodeResponse"]["result"]["geometry"]["location"]["lat"]
+        @name_to_lng[address] = parsed_res["GeocodeResponse"]["result"]["geometry"]["location"]["lng"]
       rescue
         @weird_lat = parsed_res["GeocodeResponse"]["result"]
         @weird_lng = parsed_res["GeocodeResponse"]["result"]
@@ -22,7 +24,7 @@ class SearchController < ApplicationController
         @weird_coord = "#{lat},#{lng}"
       end  
       # @coordinates = "#{lat},#{lng}"
-      return "#{lat},#{lng}" #important to use double quotes for string interp.
+      # return "#{lat},#{lng}" #important to use double quotes for string interp.
     end
 
     def time
@@ -39,36 +41,36 @@ class SearchController < ApplicationController
 
       #NEED-MODIFICATION: TAKE LIST OF CITIES AND PLACE COORD INTO GEOCODE_LOCATION
       #14 STATIC CITIES
-      @name_to_urls["Montreal, Canada"] = @query + " " + "geocode:" + "45.505730,-73.579928,2mi"
-      @name_to_urls["San Francisco, US"] = @query + " " + "geocode:" + "37.781157,-122.398720,2mi"
-      @name_to_urls["Vancouver, Canada"] = @query + " " + "geocode:" + "49.2612260,-123.1139268,2mi"
-      @name_to_urls["Toronto, Canada"] = @query + " " + "geocode:" + "43.6532260,-79.3831843,2mi"
-      @name_to_urls["New York, US"] = @query + " " + "geocode:" + "40.7056308,-73.9780035,2mi"
-      @name_to_urls["Los Angeles, US"] = @query + " " + "geocode:" + "34.0522342,-118.2436849,2mi "
-      @name_to_urls["London, England"] = @query + " " + "geocode:" + "51.5085150,-0.1254872,2mi "
-      @name_to_urls["Paris, France"] = @query + " " + "geocode:" + "48.8566140,2.3522219,2mi "
-      @name_to_urls["Berlin, Germany"] = @query + " " + "geocode:" + "52.5200066,13.4049540,2mi "
-      @name_to_urls["Hong Kong, Hong Kong"] = @query + " " + "geocode:" + "22.3964280,114.1094970,2mi "
-      @name_to_urls["Taipei, Taiwan"] = @query + " " + "geocode:" + "25.0910750,121.5598345,2mi "
-      @name_to_urls["Tokyo, Japan"] = @query + " " + "geocode:" + "35.6894875,139.6917064,2mi"
-      @name_to_urls["Seoul, South Korea"] = @query + " " + "geocode:" + "37.5665350,126.9779692,2mi"
-      @name_to_urls["Sydney, Australia"] = @query + " " + "geocode:" + "-33.8674869,151.2069902,2mi"
+      # @name_to_urls["Montreal, Canada"] = @query + " " + "geocode:" + "45.505730,-73.579928,2mi"
+      # @name_to_urls["San Francisco, US"] = @query + " " + "geocode:" + "37.781157,-122.398720,2mi"
+      # @name_to_urls["Vancouver, Canada"] = @query + " " + "geocode:" + "49.2612260,-123.1139268,2mi"
+      # @name_to_urls["Toronto, Canada"] = @query + " " + "geocode:" + "43.6532260,-79.3831843,2mi"
+      # @name_to_urls["New York, US"] = @query + " " + "geocode:" + "40.7056308,-73.9780035,2mi"
+      # @name_to_urls["Los Angeles, US"] = @query + " " + "geocode:" + "34.0522342,-118.2436849,2mi "
+      # @name_to_urls["London, England"] = @query + " " + "geocode:" + "51.5085150,-0.1254872,2mi "
+      # @name_to_urls["Paris, France"] = @query + " " + "geocode:" + "48.8566140,2.3522219,2mi "
+      # @name_to_urls["Berlin, Germany"] = @query + " " + "geocode:" + "52.5200066,13.4049540,2mi "
+      # @name_to_urls["Hong Kong, Hong Kong"] = @query + " " + "geocode:" + "22.3964280,114.1094970,2mi "
+      # @name_to_urls["Taipei, Taiwan"] = @query + " " + "geocode:" + "25.0910750,121.5598345,2mi "
+      # @name_to_urls["Tokyo, Japan"] = @query + " " + "geocode:" + "35.6894875,139.6917064,2mi"
+      # @name_to_urls["Seoul, South Korea"] = @query + " " + "geocode:" + "37.5665350,126.9779692,2mi"
+      # @name_to_urls["Sydney, Australia"] = @query + " " + "geocode:" + "-33.8674869,151.2069902,2mi"
 
       #...
 
       #2 DYNAMIC CITIES
-      unless (params[:location].blank?)
+      unless (@location.blank?)
         # if address_to_coordinates(params[:location]) == "N/A,N/A"
         #   @name_to_urls["#{params[:location]}"] = "Location not found."
         # else
-          @name_to_urls["#{params[:location]}"] = @query + " " + "geocode:" + address_to_coordinates(params[:location]) + ",2mi"
+          @name_to_urls["#{params[:location]}"] = @query + " " + "geocode:" + "#{@name_to_lat[@location]},#{@name_to_lng[@location]},2mi"
         # end
       end
-      unless (params[:location1].blank?)
+      unless (@location1.blank?)
         # if address_to_coordinates(params[:location1]) == "N/A,N/A"
         #   @name_to_urls["#{params[:location1]}"] = "Location not found."
         # else 
-        @name_to_urls["#{params[:location1]}"] = @query + " " + "geocode:" + address_to_coordinates(params[:location1]) + ",2mi"
+          @name_to_urls["#{params[:location1]}"] = @query + " " + "geocode:" + "#{@name_to_lat[@location1]},#{@name_to_lng[@location1]},2mi"
         # end
       end
     end
@@ -114,11 +116,10 @@ class SearchController < ApplicationController
       end  
     end
 
-
     #Main methods
     format_query(params)
     twitter_request(client)
-  
+
     #5/11
     #tried installing google-maps-for-rail
     #completed steps including install underscore and npm (nodeJS package manager), but somehow running the server "rails s" gives me error.    
