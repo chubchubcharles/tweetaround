e03815fdb09cd9bf51ae5ba5f092b7e03b0c187b
Charles Liu <che-yuan.liu@mail.mcgill.ca>
2014-05-12 20:16:33 -0400

    twitter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -75,7 +75,7 @@ class SearchController < ApplicationController
         config.consumer_secret    = ENV["TWITTER_CONSUMER_SECRET"]
         config.access_token     = ENV["TWITTER_ACCESS_TOKEN"]
         config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-        # config.bearer_token     = ENV["TWTTER_BEARER_TOKEN"]
+        config.bearer_token     = ENV["TWTTER_BEARER_TOKEN"]
       end 
       # @client_user = client.user("charlesliu2012")
       @client_bearer_token = client.bearer_token
